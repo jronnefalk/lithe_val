bfaa148cc379655f2343be38d6c5fcda8defdd85
MilaShahnavaz <milsh107@student.liu.se>
2023-03-01 10:01:55 +0100

    Update handlesubmit.js

--- src/handles/handlesubmit.js
+++ src/handles/handlesubmit.js
@@ -1,18 +1,16 @@
 import { addDoc, collection } from "@firebase/firestore"
 import { firestore } from "../firebase_setup/firebase"
- 
-const handleSubmit = (testdata) => {
-    const ref = collection(firestore, "test_data") // Firebase creates this automatically
- 
-    let data = {
-        testData: testdata
-    }
-    
-    try {
-        addDoc(ref, data)
-    } catch(err) {
-        console.log(err)
-    }
+
+const handleSubmit = (kursnamn) => {
+    const ref = collection(firestore, "masterkurser") // Firebase creates this automatically
+    let data = {
+    kursnamn: kursnamn
+    }
+
+    try {
+        addDoc(ref, data)
+    } catch(err) {
+    console.log(err)
+    }
 }
- 
 export default handleSubmit

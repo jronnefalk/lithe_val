e3430b0a52e4f8820ff7987d7a6ffdab44da389f
jronnefalk <julia@ronnefalk.se>
2023-03-01 14:28:50 +0100

    fil för att testa hämta data

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +18,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);

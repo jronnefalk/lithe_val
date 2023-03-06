ab57731e07906df52c91d7cc236bed01f08b5812
rasmussvala <rassv453@student.liu.se>
2023-03-06 13:41:28 +0100

    Update App.js
    uppdaterat App.js så att den importerar .json och printar all dess data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import kurser from "./webscraping/database.json";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hej från App</h1>
+      {kurser.map((kurs) => (
+        <>
+          <h1>{kurs.kursnamn}</h1>
+          <p>Kurskod: {kurs.kurskod}</p>
+          <p>HP: {kurs.hp}</p>
+          <p>Huvudområde: {kurs.huvudomrade[0]}</p>
+          <p>Utbildningsnivå: {kurs.utbildningsniva}</p>
+          <p>Termin: {kurs.termin}</p>
+          <p>Period: {kurs.period[0]}</p>
+          <p>Block: {kurs.block[0]}</p>
+          <p>Ort: {kurs.ort}</p>
+          <a href={kurs.url}>Kurshemsida</a>
+        </>
+      ))}
     </div>
   );
 }

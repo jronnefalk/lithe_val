92c9dc7f03f6c8290037cbc7e649949c8837f737
Emma Zettervall <emmze999@student.liu.se>
2023-03-07 09:33:15 +0100

    Update App.js
    sök-funk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import kurser from "./webscraping/database.json";
+import {useState} from "react";
+
 
 function App() {
+  const [query, setQuery] = useState("")
   return (
+    
     <div className="App">
-      {kurser.map((kurs) => (
-        <>
+         <div>
+         <input placeholder="Sök" onChange={event => setQuery(event.target.value)} />
+    </div>
+    {kurser.filter(kurs => {
+    if (query === '') {
+      return kurs;
+    } else if (kurs.kursnamn.toLowerCase().includes(query.toLowerCase())) {
+      return kurs;
+    } else if(kurs.kurskod.toLowerCase().includes(query.toLowerCase())) {
+      return kurs;}
+  })
+.map((kurs) => (
+        <div className="box" key={kurs.id}>
           <h1>{kurs.kursnamn}</h1>
           <p>Kurskod: {kurs.kurskod}</p>
           <p>HP: {kurs.hp}</p>
@@ -16,7 +31,7 @@ function App() {
           <p>Block: {kurs.block[0]}</p>
           <p>Ort: {kurs.ort}</p>
           <a href={kurs.url}>Kurshemsida</a>
-        </>
+          </div>
       ))}
     </div>
   );

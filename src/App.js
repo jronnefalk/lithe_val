1b12497e1e1b11ba5ab82922acb968375db15285
rasmussvala <rassv453@student.liu.se>
2023-03-07 09:20:17 +0100

    Skapat en kurs-komponent
    gjorde det för att få bort kod från App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
 import kurser from "./webscraping/database.json";
+import Kurs from "./Kurs";
 
 function App() {
   return (
     <div className="App">
       {kurser.map((kurs) => (
-        <>
-          <h1>{kurs.kursnamn}</h1>
-          <p>Kurskod: {kurs.kurskod}</p>
-          <p>HP: {kurs.hp}</p>
-          <p>Huvudområde: {kurs.huvudomrade[0]}</p>
-          <p>Utbildningsnivå: {kurs.utbildningsniva}</p>
-          <p>Termin: {kurs.termin}</p>
-          <p>Period: {kurs.period[0]}</p>
-          <p>Block: {kurs.block[0]}</p>
-          <p>Ort: {kurs.ort}</p>
-          <a href={kurs.url}>Kurshemsida</a>
-        </>
+        <Kurs key={kurs.kurskod} kursdata={kurs} />
       ))}
     </div>
   );
---
+++ src/Kurs.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Kurs(props) {
+  const kurs = props.kursdata;
+  return (
+    <>
+      <h1>{kurs.kursnamn}</h1>
+      <p>Kurskod: {kurs.kurskod}</p>
+      <p>HP: {kurs.hp}</p>
+      <p>Huvudområde: {kurs.huvudomrade[0]}</p>
+      <p>Utbildningsnivå: {kurs.utbildningsniva}</p>
+      <p>Termin: {kurs.termin}</p>
+      <p>Period: {kurs.period[0]}</p>
+      <p>Block: {kurs.block[0]}</p>
+      <p>Ort: {kurs.ort}</p>
+      <a href={kurs.url}>Kurshemsida</a>
+    </>
+  );
+}

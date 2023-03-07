94bfd1ad9e6b81972e325630df9a33d0887599b1
rasmussvala <rassv453@student.liu.se>
2023-03-07 10:12:05 +0100

    flyttat ut sökfunktionen till en separat fil

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React from "react";
 import kurser from "./webscraping/database.json";
-import {useState} from "react";
-
+import { useState } from "react";
 import Kurs from "./Kurs";
+import filterKurser from "./functions/filterKurser";
 
 function App() {
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useState("");
+  const filteredKurser = filterKurser(kurser, query);
+
   return (
-    
     <div className="App">
-         <div>
-         <input placeholder="Sök" onChange={event => setQuery(event.target.value)} />
-    </div>
-    {kurser.filter(kurs => {
-    if (query === '') {
-      return kurs;
-    } else if (kurs.kursnamn.toLowerCase().includes(query.toLowerCase())) {
-      return kurs;
-    } else if(kurs.kurskod.toLowerCase().includes(query.toLowerCase())) {
-      return kurs;}
-  })
-.map((kurs) => (
-        <div className="box" key={kurs.id}>
-          <h1>{kurs.kursnamn}</h1>
-          <p>Kurskod: {kurs.kurskod}</p>
-          <p>HP: {kurs.hp}</p>
-          <p>Huvudområde: {kurs.huvudomrade[0]}</p>
-          <p>Utbildningsnivå: {kurs.utbildningsniva}</p>
-          <p>Termin: {kurs.termin}</p>
-          <p>Period: {kurs.period[0]}</p>
-          <p>Block: {kurs.block[0]}</p>
-          <p>Ort: {kurs.ort}</p>
-          <a href={kurs.url}>Kurshemsida</a>
-          </div>
-      {kurser.map((kurs) => (
+      <div>
+        <input
+          placeholder="Sök"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+      {filteredKurser.map((kurs) => (
         <Kurs key={kurs.kurskod} kursdata={kurs} />
       ))}
     </div>

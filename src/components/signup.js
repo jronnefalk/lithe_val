d1c8543ccad0d9f789c47fa861f1a4fbc66805cf
MilaShahnavaz <milsh107@student.liu.se>
2023-03-20 15:11:34 +0100

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,48 +1,64 @@
 
 import app from "../firebase_setup/firebase.js";
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import React, { useState,useEffect } from "react";
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
 
 var provider = new GoogleAuthProvider();
 const auth = getAuth();
 
 function GoogleAuth() {
- 
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+  }, []);
+
   function googleSignin() {
     provider.setCustomParameters({ prompt: 'select_account' });
       signInWithPopup(auth, provider)
       .then(function(result){
         const credential = GoogleAuthProvider.credentialFromResult(result);
         var token = credential.accessToken;
-        var user = result.user;
-        
+        setLoggedIn(true);
         console.log(token);
-        console.log(user);
+        
       })
       .catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        //const credential = GoogleAuthProvider.credentialFromError(error);
 
         console.log(errorCode);
         console.log(errorMessage);
       });
   }
-  function googleSignout() {
-    app.Auth()
-      .signOut()
-      .then(function() {
-        console.log('Signout Successful');
-      })
-      .catch(function(error) {
-        console.log('Signout Failed:', error.message);
-      });
-  }
-  return (
-    <>
-      <button onClick={googleSignin}>Google Signin</button>
-      <button onClick={googleSignout}>Google Signout</button>
-    </>
-  );
+      function googleSignout() {
+        if (auth.currentUser) {
+          signOut(auth)
+            .then(function() {
+              console.log('Signout Successful');
+              setLoggedIn(false);
+            })
+            .catch(function(error) {
+              console.log('Signout Failed:', error.message);
+            });
+        } else {
+          console.log('No user signed in');
+        }
+      }
+
+      return (
+        <>
+          {!loggedIn && <button onClick={googleSignin}>Google Signin</button>}
+          {loggedIn && <button onClick={googleSignout}>Google Signout</button>}
+        </>
+          );
 }
 
 export default GoogleAuth;

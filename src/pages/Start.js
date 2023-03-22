e9707e91be43bf2e261960449da580f664f7edd8
rasmussvala <rassv453@student.liu.se>
2023-03-22 09:51:49 +0100

    Update Start.js
    flyttat mina grejer till Start.js

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,34 +1,73 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//import { BrowserRouter, Route } from "react-router-dom";
 
 // Data
-import kurser from "../webscraping/database.json";
+import kurser from "./webscraping/database.json";
 
 // Funktioner
-import filterKurser from "../functions/filterKurser";
+import filterKurser from "./functions/filterKurser";
 
 // Komponenter
-import Kurs from "../components/Kurs";
-import Filters from "../components/Filters";
+import Kurs from "./components/Kurs";
+//import Filters from "./components/Filters";
 
-export function Start() {
+function App() {
+  const [query, setQuery] = useState("");
+  const [activeFilters, setActiveFilters] = useState([]);
 
-    const [query, setQuery] = useState("");
-    const filteredKurser = filterKurser(kurser, query);
+  const filters = [
+    { label: "Period 1", key: "period", value: "1" },
+    { label: "Period 2", key: "period", value: "2" },
+    { label: "Block 1", key: "block", value: "1" },
+    { label: "Block 2", key: "block", value: "2" },
+    { label: "Block 3", key: "block", value: "3" },
+  ];
 
-    return (<div className="App">
-    <div>
-      <input
-        placeholder="Sök"
-        onChange={(event) => setQuery(event.target.value)}
-      />
+  // Funktion som hanterar när användaren klickar på ett filter
+  const handleFilterChange = (filter) => {
+    if (activeFilters.includes(filter)) {
+      // Om filter redan är aktiverat, ta bort det
+      setActiveFilters(activeFilters.filter((f) => f !== filter));
+    } else {
+      // Om filter inte är aktiverat, aktivera det
+      setActiveFilters([...activeFilters, filter]);
+    }
+  };
+
+  // Logga aktiva filter i konsolen (DEBUG)
+  useEffect(() => {
+    console.log(activeFilters);
+  }, [activeFilters]);
+
+  const filteredKurser = filterKurser(kurser, query, activeFilters);
+
+  return (
+    <div className="App">
       <div>
-        <Filters />
+        <input
+          placeholder="Sök"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+
+      <div>
+        {filters.map((filter) => (
+          <label key={filter.label}>
+            <input
+              type="checkbox"
+              onChange={() => handleFilterChange(filter)}
+              checked={activeFilters.includes(filter)}
+            />
+            {filter.label}
+          </label>
+        ))}
+      </div>
+
+      <div>
+        {filteredKurser.map((kurs) => (
+          <Kurs key={kurs.kurskod} kursdata={kurs} />
+        ))}
       </div>
     </div>
-    {filteredKurser.map((el) => (
-      <Kurs kursdata={el} />
-    ))}
-
-  </div>
   );
 }

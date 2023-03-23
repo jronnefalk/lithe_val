e25886129418805873c18605f8cf8edf6c73fe12
Alice <alihe684@student.liu.se>
2023-03-23 10:54:15 +0100

    början på header

--- src/App.js
+++ src/App.js
@@ -4,36 +4,61 @@ import { Link, Route, Routes } from "react-router-dom";
 import { MinSida } from "./pages/MinSida";
 import { Start } from "./pages/Start";
 
-//Kurser
+//design
 
-import "./design/kurser.css";
+import "./design/meny.css";
+
+//icons
+import { BsHouseDoorFill } from "react-icons/bs";
+import { BsBookmark } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
 
 function App() {
-
   return (
-  
     <>
-    <nav>
-      <ul>
-        <li>
-          {/* state={} if we want to pass info */}
-          <Link to="/">Start</Link>
-        </li>
-        <li>
-          <Link to="/minasidor">Min Sida</Link>
-        </li>
-      </ul>
-    </nav>
+      <div class="menybar">
+        {/* state={} if we want to pass info */}
+        <div class="start">
+          <Link to="">
+            {" "}
+            <span>
+              {" "}
+              <BsHouseDoorFill
+                size={28}
+                style={{ padding: 0, margin: 0 }}
+              />{" "}
+              <h1 class="text">Startsida</h1>
+            </span>
+          </Link>
+        </div>
 
-    <Routes>
-      <Route path="/" element={<Start />} />
-      <Route path="/minasidor" element={<MinSida />} />
+        <div class="minasidor">
+          <Link to="/minasidor">
+            <span>
+              {" "}
+              <BsBookmark size={28} style={{ padding: 0, margin: 0 }} />{" "}
+              <h1 class="text">Min sida</h1>
+            </span>
+          </Link>
+        </div>
 
-    </Routes>
+        <div class="loggain">
+          <Link to="/loggain">
+            <span class="loggain">
+              {" "}
+              <BsPerson size={32} style={{ padding: 0, margin: 0 }} />{" "}
+              <h1 class="text">Logga in</h1>
+            </span>
+          </Link>
+        </div>
+      </div>
+      h1.style.margin = "0";
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/minasidor" element={<MinSida />} />
+      </Routes>
     </>
-    
-  )
-  
+  );
 }
 
 export default App;
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -11,39 +11,39 @@ import Kurs from "../components/Kurs";
 import Filters from "../components/Filters";
 
 // inkoner
-import{ BsSearch } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
-//design 
-import '../design/filter.css';
-import '../design/kurser.css';
-import '../design/sök.css';
-
+//design
+import "../design/filter.css";
+import "../design/kurser.css";
+import "../design/sök.css";
+//import "./design/meny.css";
 
 export function Start() {
+  const [query, setQuery] = useState("");
+  const filteredKurser = filterKurser(kurser, query);
 
-    const [query, setQuery] = useState("");
-    const filteredKurser = filterKurser(kurser, query);
-
-    return (<div className="App">
-     
+  return (
+    <div className="App">
       <div className="sök">
-        <span className="sök_ikon"><BsSearch size={12}/></span>
-      <input className="sök_text" 
-        type="text"
-        placeholder= "Sök"
-        onChange={(event) => setQuery(event.target.value)}
-      />
+        <span className="sök_ikon">
+          <BsSearch size={12} />
+        </span>
+        <input
+          className="sök_text"
+          type="text"
+          placeholder="Sök"
+          onChange={(event) => setQuery(event.target.value)}
+        />
       </div>
       <div className="filter">
         <p>Filter</p>
         <Filters />
       </div>
-      
-    
-    {filteredKurser.map((el) => (
-      <Kurs kursdata={el} />
-    ))}
 
-  </div>
+      {filteredKurser.map((el) => (
+        <Kurs kursdata={el} />
+      ))}
+    </div>
   );
 }

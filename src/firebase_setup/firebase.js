f470bb094278d4a182164e4f2dd9a97cb46d6250
Emma Zettervall <emmze999@student.liu.se>
2023-03-29 11:33:05 +0200

    Update firebase.js
    tog bort import av get och child

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push, remove, get, child } from "firebase/database";
+import { getDatabase, ref, push, remove } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use

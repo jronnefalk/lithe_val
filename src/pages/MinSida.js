6c955952e78f4c51c9d04a9a01bdfbb63867bb08
jronnefalk <julia@ronnefalk.se>
2023-03-31 08:46:33 +0200

    Lagt till funktion getData
    Denna funktion hämtar kursinfo från vår json fil om vi har en kurskoden. Även strukturerat upp och namngett taggarna i firebase

--- src/pages/MinSida.js
+++ src/pages/MinSida.js
@@ -1,11 +1,19 @@
-import React from "react";
+import getData from "../functions/getData";
+import Kurs from "../components/Kurs";
+import { v4 as uuidv4 } from "uuid"; // Key generator for React komponenter
 
 export function MinSida() {
+  const kurs = "TSBK38";
+  const getCourseData = getData(kurs);
+
   return (
     <div>
-      <h2>Mina valda kurser</h2>
+      <h1>My Courses</h1>
+      <div>
+        {getCourseData.map((kurs) => (
+          <Kurs key={uuidv4()} kursdata={kurs} />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default MinSida;

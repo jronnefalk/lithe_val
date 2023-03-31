2aa4e334dba025f3149a7019b3ffb09241022699
jronnefalk <julia@ronnefalk.se>
2023-03-31 09:12:02 +0200

    minor changes

--- src/pages/MinSida.js
+++ src/pages/MinSida.js
@@ -3,8 +3,9 @@ import Kurs from "../components/Kurs";
 import { v4 as uuidv4 } from "uuid"; // Key generator for React komponenter
 
 export function MinSida() {
-  const kurs = "TSBK38";
-  const getCourseData = getData(kurs);
+  // key ska sen ändras till att matcha kurskoden som hämtas från firebase
+  const key = "TNM098";
+  const getCourseData = getData(key);
 
   return (
     <div>

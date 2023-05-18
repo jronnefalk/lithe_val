ef477187205cdd042bf024a7973633cfb700cd0e
Emma Zettervall <emmze999@student.liu.se>
2023-05-18 14:43:33 +0200

    bytt färg på cirkel och lagt till lite mellanslag

--- src/components/Visualisering.js
+++ src/components/Visualisering.js
@@ -64,7 +64,7 @@ export default function Schema(props) {
         <Progressbarochrubrik>
           <Bubble>
             <Titel>
-              Poäng inom avancerade kurser: {counts.avancerad * 6}/60hp
+              Poäng inom avancerade kurser: {counts.avancerad * 6}/60 hp
             </Titel>
             <OmsluterBubble>
               <Progressbar value={avanceradPercent} max="12"></Progressbar>
@@ -75,7 +75,7 @@ export default function Schema(props) {
 
             <Titel>
               Poäng inom avancerade kurser i Medieteknik:{" "}
-              {counts.medieteknik * 6}/30hp
+              {counts.medieteknik * 6}/30 hp
             </Titel>
             <OmsluterBubble>
               <Progressbar value={medieteknikPercent} max="6"></Progressbar>
@@ -86,7 +86,7 @@ export default function Schema(props) {
 
             <Titel>
               Poäng inom avancerade kurser i Datateknik: {counts.datateknik * 6}
-              /30hp
+              /30 hp
             </Titel>
             <OmsluterBubble>
               <Progressbar value={datateknikPercent} max="6"></Progressbar>
@@ -97,7 +97,7 @@ export default function Schema(props) {
           </Bubble>
         </Progressbarochrubrik>{" "}
         <CirkelWrapper>
-          <Titel>Totalpoäng: {counts.hp}/90hp</Titel>
+          <Titel>Totalpoäng: {counts.hp}/90 hp</Titel>
           <Circle hp={hpPercent}></Circle>
         </CirkelWrapper>
       </Visualiseringar>

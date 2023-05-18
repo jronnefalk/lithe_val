c9e3b5e38395e7c7296a5a693d58f7f4e17c7271
Emma Zettervall <emmze999@student.liu.se>
2023-05-18 14:31:24 +0200

    laggt till ! till överlappskurser

--- src/components/Kurs.js
+++ src/components/Kurs.js
@@ -42,6 +42,7 @@ import { AiOutlineUp } from "react-icons/ai";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { BsFolderPlus } from "react-icons/bs";
 import { BsTrash3 } from "react-icons/bs";
+import { BsExclamationLg } from "react-icons/bs";
 
 export default function Kurs(props) {
   const { kursdata, FireBaseData } = props;
@@ -130,7 +131,20 @@ export default function Kurs(props) {
   return (
     <>
       <TitelKnappCont>
-        <Titel>{kurs.kursnamn}</Titel>
+        <Titel>
+          {kurs.kursnamn}
+          {hasOverlappning && (
+            <span
+              style={{
+                color: "rgb(255, 92, 100)",
+                paddingLeft: "15px",
+                fontSize: "22px",
+              }}
+            >
+              !
+            </span>
+          )}
+        </Titel>
 
         {isInFirebase ? (
           <TaBort onClick={handleDelete}>
@@ -227,10 +241,13 @@ export default function Kurs(props) {
       )}
       {isReadMore && hasOverlappning && (
         <TextUnderLäsMer>
-          <span style={{ fontWeight: "bold" }}>
-            Kursen får ej ingå i examen tillsammans med
+          <span style={{ fontWeight: "bold", color: "rgb(255, 92, 100)" }}>
+            Kursen får ej ingå i examen tillsammans med:
           </span>{" "}
-          {kurs.overlappning.join(", ")}
+          <span style={{ color: "rgb(255, 92, 100)" }}>
+            {" "}
+            {kurs.overlappning.join(", ")}{" "}
+          </span>
         </TextUnderLäsMer>
       )}
       {isReadMore && (

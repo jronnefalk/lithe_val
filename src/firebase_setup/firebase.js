634698e76abebfb1f40d7e6774891001ff7cf77d
jronnefalk <julia@ronnefalk.se>
2023-05-24 21:41:28 +0200

    testar vercel

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -10,7 +10,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCNXZdYpF7C82lV5hOlre-jWoLDosNLdjY",
-  authDomain: "liteval.firebaseapp.com",
+  authDomain: "litheval.vercel.app",
   databaseURL: "https://liteval-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "liteval",
   storageBucket: "liteval.appspot.com",

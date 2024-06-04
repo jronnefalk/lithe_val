310aad712735738dfd27501343ec7613d6eaef8b
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-06-04 15:08:49 +0200

    Updated firebase.js

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -9,14 +9,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "liteval.firebaseapp.com",
+  databaseURL: "https://liteval-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "liteval",
+  storageBucket: "liteval.appspot.com",
+  messagingSenderId: "337252383270",
+  appId: "1:337252383270:web:dbafe11167ecc773ce84e7",
+  measurementId: "G-44PFP6SK89",
 };
 
 // Initialize Firebase
